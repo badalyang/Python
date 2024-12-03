abe20338b129ee56185bd5f8e04ac5f8c8129f09
badalyang <126382208+badalyang@users.noreply.github.com>
2024-12-03 18:04:54 +0400

    main.py

--- lec12/main.py
+++ lec12/main.py
@@ -1,34 +1,6 @@
+
 import random
 import time
-
-def add_nums(name):
-    file_w = open(name, "w")
-    for line in range(100):
-        for number in range(20):
-            file_w.write(str(random.randint(0, 100)))
-            file_w.write(" ")
-        file_w.write("\n")
-    file_w.close()
-
-def int_arr(name):
-    file_r = open(name, 'r')
-    lines = file_r.readlines()
-    file_r.close()
-    file_w = open(name, 'w')
-    for line in lines:
-        arr = list(map(int, line.strip().split()))
-        filtered_arr = list(filter(lambda x: x > 40, arr))
-        print(filtered_arr)
-        file_w.write(" ".join(map(str, filtered_arr)) + "\n")
-    file_w.close()
-
-def read_file_with_yield(name):
-    with open(name, 'r') as file_r:
-        for line in file_r:
-            yield line.strip()
-print("Reading with YEILD\n")
-for line in read_file_with_yield("111.txt"):
-    print(line)
 
 def measure_time(func):
     def wrapper(*args, **kwargs):
@@ -41,9 +13,37 @@ def measure_time(func):
     return wrapper
 
 @measure_time
-def decorated_int_arr(name):
-    int_arr(name)
-    
-int_arr("111.txt")
+def add_nums(name):
+    file_w = open(name, "w")
+    for line in range(100):
+        for number in range(20):
+            file_w.write(str(random.randint(0, 100)))
+            file_w.write(" ")
+        file_w.write("\n")
+    file_w.close()
+
+@measure_time
+def int_arr(name):
+    file_r = open(name, 'r')
+    lines = file_r.readlines()
+    file_r.close()
+    file_w = open(name, 'w')
+    for line in lines:
+        arr = list(map(int, line.strip().split()))
+        filtered_arr = list(filter(lambda x: x > 40, arr))
+        print(filtered_arr)
+        file_w.write(" ".join(map(str, filtered_arr)) + "\n")
+    file_w.close()
+
+@measure_time
+def read_file_with_yield(name):
+    with open(name, 'r') as file_r:
+        for line in file_r:
+            yield line.strip()
+
+print("Reading with YEILD\n")
+for line in read_file_with_yield("111.txt"):
+    print(line)
 
 add_nums("111.txt")
+int_arr("111.txt")
